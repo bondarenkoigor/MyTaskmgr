505d12b265d1e9886b8a0dc50f42e2038b3f3be7
bondarenkoigor <Bond_fh54@student.itstep.org>
2022-06-19 15:14:45 +0300

    Добавьте файлы проекта.

---
+++ Model/Process__Model.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace MyTaskmgr.Model
+{
+    public class Process__Model : INotifyPropertyChanged
+    {
+        public Process MyProcess { get; set; }
+        private PerformanceCounter CPUCounter { get; set; }
+
+        public Thread cpuUsageUpdate;
+
+        public Process__Model(Process process)
+        {
+            MyProcess = process;
+
+            CPUCounter = new PerformanceCounter("Process", "% Processor Time", MyProcess.ProcessName);
+            CPUCounter.NextValue();
+
+            cpuUsageUpdate = new Thread(new ThreadStart(UpdateCPU));
+            cpuUsageUpdate.IsBackground = true;
+            cpuUsageUpdate.Start();
+
+        }
+
+        private void UpdateCPU()
+        {
+            this.CPUUsage = this.CPUCounter.NextValue();
+        }
+
+        public string ProcessName { get 
+            {
+                if(MyProcess.MainWindowTitle == String.Empty) return MyProcess.ProcessName;
+                return MyProcess.MainWindowTitle;
+            } set { } }
+        public double RAMUsage { get { return MyProcess.PrivateMemorySize64 / 1000000.0; } set { } }
+
+
+        private float _CPUUsage;
+
+        public float CPUUsage
+        {
+            get { return _CPUUsage / Environment.ProcessorCount; }
+            set { _CPUUsage = value; OnPropertyChanged("CPUUsage"); }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ViewModel/MainWindow__ViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using GalaSoft.MvvmLight.Command;
+using MyTaskmgr.Model;
+using System.Threading;
+
+namespace MyTaskmgr.ViewModel
+{
+    public class MainWindow__ViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Process__Model> Processes { get; set; }
+
+        private Process__Model selectedProcess;
+
+        public Process__Model SelectedProcess
+        {
+            get { return selectedProcess; }
+            set { selectedProcess = value; OnPropertyChanged(nameof(SelectedProcess)); }
+        }
+
+        private PerformanceCounter totalCPUCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+        private PerformanceCounter totalRAMCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use", null);
+
+        public int TotalCPU
+        {
+            get { return (int)totalCPUCounter.NextValue(); }
+            private set {  }
+        }
+
+        public int TotalRAM
+        {
+            get { return (int)totalRAMCounter.NextValue(); }
+            private set { }
+        }
+
+        public MainWindow__ViewModel()
+        {
+            Thread updateProcesses = new Thread(new ThreadStart(UpdateProcesses));
+            updateProcesses.Start();
+            updateProcesses.IsBackground = true;
+
+        }
+
+        private void UpdateProcesses()
+        {
+            while (true)
+            {
+                var processList = Process.GetProcesses().OrderBy(proc => proc.ProcessName).ToList();
+                var modelList = new ObservableCollection<Process__Model>();
+                foreach (var process in processList)
+                    modelList.Add(new Process__Model(process));
+
+                Processes = modelList;
+                try { this.SelectedProcess = modelList[modelList.IndexOf(selectedProcess)]; } catch { }
+                Thread.Sleep(1000);
+                OnPropertyChanged(nameof(Processes));
+                OnPropertyChanged(nameof(TotalCPU));
+                OnPropertyChanged(nameof(TotalRAM));
+            }
+        }
+
+        RelayCommand endTaskCommand;
+
+        public RelayCommand EndTaskCommand
+        {
+            get
+            {
+                return endTaskCommand ?? (endTaskCommand = new RelayCommand(() =>
+                {
+                    try
+                    {
+                        SelectedProcess.MyProcess.Kill();
+                        SelectedProcess.cpuUsageUpdate.Abort();
+                        this.Processes.Remove(this.SelectedProcess);
+                    }
+                    catch { }
+                }));
+            }
+            set { endTaskCommand = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
